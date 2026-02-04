108f8252079fa2587e69ee695ba1366b6c4ae41a
254gerboo <116030508+254gerboo@users.noreply.github.com>
2026-02-04 22:27:56 +0300

    updatee

---
+++ settings.js
@@ -0,0 +1,34 @@
+/*I wrote a script to automate my job. Now I just sit back and watch Netflix while it runs.*/
+const fs = require('fs-extra');
+const path = require('path');
+if (fs.existsSync('set.env')) {
+    require('dotenv').config({ path: __dirname + '/set.env' });
+}
+const session = process.env.SESSION || '';
+const dev = process.env.OWNER_NUMBER || '254710537788';
+const { Sequelize } = require('sequelize'); 
+const DATABASE_URL = process.env.DATABASE_URL || './database.db'; 
+const database =
+  DATABASE_URL === './database.db'
+    ? new Sequelize({
+        dialect: 'sqlite',
+        storage: DATABASE_URL,
+        logging: false,
+      })
+    : new Sequelize(DATABASE_URL, {
+        dialect: 'postgres',
+        ssl: true,
+        protocol: 'postgres',
+        dialectOptions: {
+          ssl: { require: true, rejectUnauthorized: false },
+        },
+        logging: false,
+      });
+
+module.exports = {  
+  database,  
+  dev,
+  session,
+};
+
+//must run
